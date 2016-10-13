059dc2d775ec1134801ea875444fe3bfc21b742e
Jon Henrik Bjørnstad <jonhenrik@gmail.com>
2016-10-13 13:47:38 +0000

    Changes for loading env_graph continously into neo4j

--- Percolibrium/Percolators/python/load.sh
+++ Percolibrium/Percolators/python/load.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 neo4j stop && rm -rf ~/neo4j/neo4j-community-2.3.7/data/graph.db && neo4j start
+rm -rf data/env_graph.*
 ./env_graph.py
 
 
--- Percolibrium/Percolators/python/load_env_graph.py
+++ Percolibrium/Percolators/python/load_env_graph.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from lib.neo4j import Neo4j
-from multiprocessing import Process
 import urllib, urllib2, json, sys, os, time, pprint, time, pyinotify, glob
 
 
@@ -10,28 +9,6 @@ execfile("conf/config.conf", config)
 
 neo4j = Neo4j(config['neo4j_url'], config['neo4j_user'], config['neo4j_pass'])
 pp = pprint.PrettyPrinter(indent=4)
-res = []
-mylist = []
-
-class ProcessTransientFile(pyinotify.ProcessEvent):
-	def process_IN_MOVED_TO(self, event):
-		# We have explicitely registered for this kind of event.
-		#print '\t', event.pathname, ' -> written'
-		f = 'data/env_graph.%d' % (time.time())
-		os.rename(event.pathname, f)
-#		print time.strftime("%d/%m/%Y %H:%M:%S") + " - MOVING env_graph TO %s" % (f)
-
-#	def process_default(self, event):
-#		print 'default: ', event.maskname
-
-
-def watch_files():
-	wm = pyinotify.WatchManager()
-	notifier = pyinotify.Notifier(wm)
-	# In this case you must give the class object (ProcessTransientFile)
-	# as last parameter not a class instance.
-	wm.watch_transient_file('/home/ubuntu/.CGNgine/state/env_graph', pyinotify.ALL_EVENTS, ProcessTransientFile)
-	notifier.loop()
 
 def insert_into_db():
 	while True:
@@ -42,6 +19,7 @@ def insert_into_db():
 			with open(fn) as f:
 				l = [ i.rstrip('\n').lstrip('(').rstrip(')').split(',') for i in f ]
 				mylist.extend(l)
+				f.close()
 				os.remove(fn)
 			#res.append(mylist)
 
@@ -97,11 +75,5 @@ r = neo4j.neo4j_rest_transaction_commit(q)
 
 time.sleep(5)
 
-p = Process(target=insert_into_db)
-d = Process(target=watch_files)
-p.start()
-d.start()
+insert_into_db()
 
-print "calling thread"
-p.join()
-d.join()
